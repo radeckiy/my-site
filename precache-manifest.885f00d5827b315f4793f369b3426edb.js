c52ac38aa6d32fe084f10ff212a701e5c8f9f0af
Andrey <radeckiy98@mail.ru>
2019-09-01 04:23:08 +0500

    Updates

---
+++ precache-manifest.885f00d5827b315f4793f369b3426edb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b1746afb501e805ec8ef37a1207f8e",
+    "url": "/my-site/index.html"
+  },
+  {
+    "revision": "2b2dbf9aa9054fd9efd1",
+    "url": "/my-site/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "5c89e61d2a4387a0760d",
+    "url": "/my-site/static/js/2.72ffec10.chunk.js"
+  },
+  {
+    "revision": "2b2dbf9aa9054fd9efd1",
+    "url": "/my-site/static/js/main.ae38e73a.chunk.js"
+  },
+  {
+    "revision": "f52ddbeb94c15d5bb818",
+    "url": "/my-site/static/js/runtime~main.96580cd8.js"
+  }
+]);
